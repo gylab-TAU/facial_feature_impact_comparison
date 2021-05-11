3a5a7ede8d3f7c7ab6763e221c9ab201f4894f3f
administrator <idg7722@gmail.com>
2021-05-12 00:33:55 +0300

    Activations done and after testingg

--- representation/analysis/metrics/euclidian_distance_compare.py
+++ representation/analysis/metrics/euclidian_distance_compare.py
@@ -3,4 +3,4 @@ import numpy as np
 
 class EuclidianDistanceCompare(object):
     def compare(self, rep1: np.ndarray, rep2: np.ndarray):
-            return np.linalg.norm(rep1 - rep2)
+        return np.linalg.norm(rep1 - rep2)
